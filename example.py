643c0bbbc0345d6eacec9f4786fd71d336420ada
Nikolay Fominykh <nikolayfn@gmail.com>
2014-12-05 09:47:04 +0300

    tabs to spaces :)

--- example.py
+++ example.py
@@ -3,12 +3,12 @@ import sys
 from koans_suite.mountain import Mountain 
 
 def main(args=None):
-	if len(args) > 1:
-		path = args[1]
-	else:
-		path = "koans"
-	Mountain().walk_the_path(path)
+    if len(args) > 1:
+        path = args[1]
+    else:
+        path = "koans"
+    Mountain().walk_the_path(path)
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
 
